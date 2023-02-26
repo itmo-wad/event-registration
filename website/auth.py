25a74cadb62010cc5a8b6e7365a75638829fbb45
Ekin Gundogdu <ekin.gundogdu@ozu.edu.tr>
2023-02-26 15:56:54 +0300

    auth route change

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,17 @@
-from flask import Blueprint,render_template,session,redirect,url_for
+from flask import Blueprint, render_template, session, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/sign-in', methods=["GET", "POST"])
+
+@auth.route('auth/sign-in', methods=["GET", "POST"])
 def sign_in():
     return render_template(f'pages/auth/sign-in.html')
 
-@auth.route('/register', methods=["GET", "POST"])
+
+@auth.route('auth/register', methods=["GET", "POST"])
 def sign_up():
     return render_template(f'pages/auth/register.html')
+
 
 @auth.route('/sign-out')
 def sign_out():
